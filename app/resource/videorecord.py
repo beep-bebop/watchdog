29a0837cccb58d5240964a76fedfff9e8c15d102
Tantalus <1132942361@qq.com>
2020-04-15 19:59:06 +0800

    根据时间范围选择返回信息与根据时间信息返回图片（待对接）

--- app/resource/videorecord.py
+++ app/resource/videorecord.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, request
-from flask import Response
+from flask import Response, make_response
 import cv2
 
 picturecounter = 1
@@ -14,25 +14,29 @@ class VideoRecord(Resource):
     def post(self):
         global picturecounter
 
-        if request.form.get('picturedate') and request.form.get('time'):
-            data = request.form.to_dict()
-            date = data.get('picturedate')
+        if (request.get_json())['picturedate']:
+
+            date = (request.get_json())['picturedate']
             temp = date.split("-")
-            year = temp[1]
-            day = temp[2]
+            year = temp[0]
+            day = temp[1] + temp[2]
+
 
             data = request.form.to_dict()
-            picpath = r'/root/video/username/%s/%s/%s' % (year, day, data.get('time'), str(picturecounter))
+            picpath = r'/root/video/username/%s/%s/%s/%s.jpg' % (year, day, (request.get_json())['time'], str(picturecounter))
             picturecounter += 1
             if(picturecounter > 5):
                 picturecounter = 1
                 return False
 
+            print(picpath)
             image = cv2.imread(picpath)
 
             bs = cv2.imencode(".jpg", image)[1].tobytes()
+            res = make_response(bs)
+            res.headers["Content-Type"] = "image/png"
 
-            return Response(bs, mimetype='image/jpeg')
+            return res
 
         else:
             print('error')
--- app/resource/videosavedata.py
+++ app/resource/videosavedata.py
@@ -8,20 +8,35 @@ class VideoSaveData(Resource):
         pass
 
     def post(self):
-
-        if request.form.get('recorddate'):
+        print((request.get_json()))
+        print((request.get_json())['recorddate'])
+        print((request.get_json())['recordminute'])
+        # if request.form.get('recorddate') and request.form.get('recordminute'):
+        if((request.get_json())['recorddate'] and (request.get_json())['recordminute']):
 
             #获得数据并处理成2020(int),0402(int)的形式
-            data = request.form.to_dict()
-            date = data.get('recorddate')
-            temp = date.split(",")
-            startdate = temp[0].split("-")
-            enddate = temp[1].split("-")
+            # data = request.form.to_dict()
+            # date = data.get('recorddate')
+            date = (request.get_json())['recorddate']
+            datetemp = date.split(",")
+            startdate = datetemp[0].split("-")
+            enddate = datetemp[1].split("-")
 
             startyear = int(startdate[0])
             startday = int(startdate[1] + startdate[2])
             endyear = int(enddate[0])
             endday = int(enddate[1] + enddate[2])
+            print("start:" + str(startyear) + str(startday) + " end:" + str(endyear) + str(endday))
+
+            minute = (request.get_json())['recordminute']
+            #minute = data.get('recordminute')
+            minutetemp = minute.split(",")
+            start = minutetemp[0].split(':')
+            end = minutetemp[1].split(':')
+            
+            startminute = int(start[0] + start[1])
+            endminute = int(end[0] + end[1])
+
 
             #对os.walk(rootpath)下所有结果的收集以及提取需要的列表作为collect
             collectroot = []
@@ -33,12 +48,16 @@ class VideoSaveData(Resource):
                 collectroot.append(time)
 
             for menber in collectroot:
+
+                menber[1].sort()
+                print(menber[1])
+
                 collect.append(menber[1])
 
             #初始化返回用的字典result
             result = {}
-            #初始化输出元素标识以及各列表和列表指针
-            resultindex = 1
+            resultlist = []
+            #初始化输出各列表和列表指针
             datapointer = 1
             yearlist = collect[0]
             daylist = None
@@ -48,6 +67,7 @@ class VideoSaveData(Resource):
             timepointer = None
             # 进行加入操作的许可
             permission = False
+
 
             while (True):
 
@@ -76,22 +96,106 @@ class VideoSaveData(Resource):
                         continue
 
                 #由指针指向的时间是否在需求时间内发放permission
+                #年份大等于开始年份
                 if (int(yearlist[yearpointer]) >= startyear):
+                    #年份为开始年份
                     if (int(yearlist[yearpointer]) == startyear):
-                        if (int(daylist[daypointer]) >= startday):
-                            permission = True
+                        #筛选年份相同
+                        if(startyear == endyear):
+                            if(int(daylist[daypointer]) >= startday):
+                                #日期等于开始日期
+                                if(int(daylist[daypointer]) == startday):
+                                    #筛选日期相同
+                                    if(startday == endday):
+                                        #大于开始时间
+                                        if(int(timelist[timepointer]) >= startminute):
+                                            if(int(timelist[timepointer]) <= endminute):
+                                                permission = True
+                                            #超出结束时间，停止读取数据
+                                            else:
+                                                permission = False
+                                                break
+                                        else:
+                                            permission = False
+                                    #筛选日期不同
+                                    else:
+                                        #时间大等于开始时间
+                                        if(int(timelist[timepointer]) >= startminute):
+                                            permission = True
+                                        else:
+                                            permission = False
+
+                                #日期不等于开始日期但小于结束日期（筛选日期不同）
+                                elif(int(daylist[daypointer]) <= endday):
+                                    #日期等于结束日期
+                                    if(int(daylist[daypointer]) == endday):
+                                        if(int(timelist[timepointer]) <= endminute):
+                                            permission = True
+                                        else:
+                                            permission = False
+                                            #超过最后一天的时间范围可以退出
+                                            break
+                                    #日期小于结束日期
+                                    else:
+                                        permission = True
+
+                                #日期大于开始日期但在范围外，退出
+                                else:
+                                    permission = False
+                                    break
+
+                            #日期小于开始日期
+                            else:
+                                permission = False
+                        #筛选年份不同
                         else:
-                            permission = False
+                            #是否在开始日期之后
+                            if(int(daylist[daypointer]) >= startday):
+                                #是否为开始日期
+                                if(int(daylist[daypointer]) == startday):
+                                    #是否在开始时间之后
+                                    if(int(timelist[timepointer]) >= startminute):
+                                        permission = True
+                                    else:
+                                        permission = False
+                                #在开始日期之后
+                                else:
+                                    permission = True
+                            #在开始日期之前
+                            else:
+                                permission = False
+
+                    #年份大于开始年份且小等于结束年份
                     elif (int(yearlist[yearpointer]) <= endyear):
-                        if (int(daylist[daypointer]) <= endday):
-                            permission = True
-                        #若endyear内现日期超出范围日期，直接退出循环
-                        #下方三个else可以不要
+                        #年份为结束年份
+                        if(int(yearlist[yearpointer]) == endyear):
+                            #在结束日期的范围内
+                            if(int(daylist[daypointer]) <= endday):
+                                #为结束年份的结束日期
+                                if(int(daylist[daypointer]) == endday):
+                                    #在结束时间之前
+                                    if(int(timelist[timepointer]) <= endminute):
+                                        permission = True
+                                    #超过最后一天的时间范围可以退出
+                                    else:
+                                        permission = False
+                                        break
+                                #在范围内且非结束日期
+                                else:
+                                    permission = True
+                            #为结束年份而在结束日期的范围外
+                            else:
+                                permission = False
+                                break
+                        #年份不为结束年份
                         else:
-                            break
-                            permission = False
+                            permission = True
+
+                    #年份大于开始年份但不在范围内
                     else:
-                        permission = True
+                        permission = False
+                        break
+                #年份小于开始年份（不在范围内）
                 else:
                     permission = False
 
@@ -109,13 +213,15 @@ class VideoSaveData(Resource):
 
                     # 设置返回值json中第n个time（'timen'）下'date'的值
                     arrange['date'] = str(yearlist[yearpointer]) + daystring
-                    arrange['minute'] = str(timelist[timepointer])
-                    result['time' + str(resultindex)] = arrange
+                    arrange['time'] = str(timelist[timepointer])
+                    arrange['name'] = 'name'
+                    arrange['address'] = 'address'
+
+                    resultlist.append(arrange)
 
                 # 指针数据的改变
                 timepointer += 1
                 datapointer += 1
-                resultindex += 1
 
                 # 若time指针超出timelist，则将pointer与list全部归空，重新进行初始化,并读下一个日期
                 if (timepointer + 1 > len(timelist)):
@@ -132,8 +238,13 @@ class VideoSaveData(Resource):
                         if (yearpointer + 1 > len(yearlist)):
                             break
 
-                # 循环结束，结果json化
-                return json.dumps(result)
+
+
+            result['tables'] = resultlist
+            # 循环结束，结果json化
+            print(json.dumps(result))
+
+            return json.dumps(result)
 
         else:
             print('error')
